6895de4ceefe87fe18fca4c5c15807e10eaffd39
v6iox <71297540+v6iox@users.noreply.github.com>
2020-10-07 15:58:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*INSERT GROUP ID AND COOKIE BELOW*/
 
-var groupId = 12345 // << Replace 12345 with your Group Id
+var groupId = 5298883 // << Replace 12345 with your Group Id
 var cookie = process.env.cookie // << Put your account cookie inside of the quotes
 
 /*INSERT GROUP ID AND COOKIE ABOVE*/
